f9ae31aaa96107f011ded443c7f9504f08bd249e
trance_zz <10878211@qq.com>
2019-06-03 18:15:22 +0800

    update

---
+++ airport-log/src/main/java/com/br/log/InterfaceLogAspect.java
@@ -0,0 +1,16 @@
+package com.br.log;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 接口日志切面
+ * @Author Zero
+ * @Date 2019 06 03
+ */
+
+@Aspect
+@Component
+public class InterfaceLogAspect  {
+
+}
