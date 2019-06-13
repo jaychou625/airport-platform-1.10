49b19bbe34f748fa6ee45ad8b1d43284742d13c7
peterpan <56650666@qq.com>
2019-06-13 09:24:52 +0800

    first

--- airport-web/src/main/java/com/br/AirportWebApplication.java
+++ airport-web/src/main/java/com/br/AirportWebApplication.java
@@ -1,10 +1,18 @@
 package com.br;
 
+import com.br.entity.map.Car;
+import com.br.entity.map.CarInfo;
+import com.br.entity.task.TaskStateInfo;
+import com.br.service.task.TrafficTaskStateService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,6 +21,25 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class AirportWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirportWebApplication.class, args);
+    }
+
+    @Autowired
+    TrafficTaskStateService trafficTaskStateService;
+
+    @Scheduled(fixedRate = 1000)
+    public void todo(){
+        Car car = new Car();
+        car.setCarType("摆渡车");
+        car.setCarNo("摆渡车No1");
+        car.setCarSeq(1);
+        CarInfo carInfo = new CarInfo();
+        carInfo.setCar(car);
+        TaskStateInfo taskState = trafficTaskStateService.getTaskState(carInfo,false);
+        if(taskState.getState() != -1){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String endTime = taskState.getState() == 2 ?  sdf.format(taskState.getEndTime()) : "无结束时间";
+            System.out.println(taskState.getId() + "---" + taskState.getCarType() + "---" + taskState.getFltNo() + "---" + taskState.getCarNo() + "---" + taskState.getDriverName() + "---" + taskState.getStartTime() + "---" + endTime + "---" + taskState.getState());
+        }
     }
 }
 
